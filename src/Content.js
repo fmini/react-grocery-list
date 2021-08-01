ebbfeffc170b4723093181fbb7c59507da8ee0d3
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-08-01 11:47:37 -0700

    imported and used useState hook to set initial state of name and change the State when setName is called

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,13 @@
-//import React from 'react';
+import { useState } from 'react';
 
 const Content = () => {
+  /* const [whatever you are treacking the state of, function to set the state when change happens = useState('sets the initial state')] */
+  const [name, setName] = useState('Dave');
   const handleNameChange = () => {
     const names = ['Bob', 'Kevin', 'Dave'];
     const int = Math.floor(Math.random() * 3);
-    return names[int];
+    /* passes the information for setting the state to the setName function which will change the current state */
+    setName(names[int]);
   };
 
   const handleClick = () => {
@@ -15,21 +18,12 @@ const Content = () => {
     console.log(`${name} was clicked`);
   };
 
-  const handleClick3 = e => {
-    console.log(e.target.innerText);
-  };
-
   return (
     <main>
-      {/* // using onDoubleClick on the <p> element </p> to run handleClick */}
-      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
-      {/* // using onClick to run handleClick on top button */}
-      <button onClick={handleClick}>Click it</button>
-      {/* // passing 'Dave' from insinde the handler to handleClick2 */}
+      <p onDoubleClick={handleClick}>Hello {name}</p>
+      {/* // using onClick to run handleNameChange on top button */}
+      <button onClick={handleNameChange}>Change Name</button>
       <button onClick={() => handleClick2('Dave')}>Click it</button>
-      {/* // grabbing the event from handleClick3 and passing part of it into the 
-      handleClcik3 handler*/}
-      <button onClick={e => handleClick3(e)}>Click it</button>
     </main>
   );
 };
