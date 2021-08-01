af5155d17a55fa8a26a7810dbd3d670f2396fc9f
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-08-01 10:06:30 -0700

    created header, footer, and content components and imported them into app.js

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+//rafce snippet will create the react arrow function component
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Grocery List</h1>
+    </header>
+  );
+};
+
+export default Header;
