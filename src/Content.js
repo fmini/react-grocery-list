d29a637e2f9f5ad980c37b1c0e081d652c48a7d9
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-08-01 11:17:13 -0700

    Added 3 click events for testing and learning

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 
 const Content = () => {
   const handleNameChange = () => {
@@ -7,9 +7,29 @@ const Content = () => {
     return names[int];
   };
 
+  const handleClick = () => {
+    console.log('you clicked it');
+  };
+
+  const handleClick2 = name => {
+    console.log(`${name} was clicked`);
+  };
+
+  const handleClick3 = e => {
+    console.log(e.target.innerText);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChange()}</p>
+      {/* // using onDoubleClick on the <p> element </p> to run handleClick */}
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+      {/* // using onClick to run handleClick on top button */}
+      <button onClick={handleClick}>Click it</button>
+      {/* // passing 'Dave' from insinde the handler to handleClick2 */}
+      <button onClick={() => handleClick2('Dave')}>Click it</button>
+      {/* // grabbing the event from handleClick3 and passing part of it into the 
+      handleClcik3 handler*/}
+      <button onClick={e => handleClick3(e)}>Click it</button>
     </main>
   );
 };
