7e1b09144de5f670806619772ae800661858d9b6
Frank Minichiello <9028036fmini@users.noreply.github.com>
2021-08-02 15:05:27 -0700

    Added input form to project in AddItem component and imorted and added it in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+// added AddItem import line 3 and AddItem component line 44
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
@@ -39,6 +41,7 @@ function App() {
   return (
     <div className="App">
       <Header title="Grocery List" />
+      <AddItem />
       <Content
         items={items}
         handleCheck={handleCheck}
